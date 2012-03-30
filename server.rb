3283d2d84a5fdc4ec11bcc61580e3d61bd4c87f1
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-03-30 19:09:34 +0300

    Add pid_file parameter

--- server.rb
+++ server.rb
@@ -1,11 +1,13 @@
 #server receive a file from client
 require 'socket'
-require 'timeout'
 
 CHUNK_SIZE = 4096 # bytes (4Kb)
 
 file = ARGV[0] # full path to file
 port = Integer(ARGV[1]) # socket's port
+pid_file = ARGV[2] # full path to pid file
+File.open(pid_file, 'w') { |file| file.write Process.pid }
+
 server = TCPServer.open(port)
 client = server.accept
 File.open(file, 'ab') do |file|
