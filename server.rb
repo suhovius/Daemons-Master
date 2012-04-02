f58537ecb2199c0dc0f3aa702241e216a3aac811
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-02 14:54:57 +0300

    Add logging for daemon

--- server.rb
+++ server.rb
@@ -9,10 +9,18 @@ pid_file = ARGV[2] # full path to pid file
 File.open(pid_file, 'w') { |file| file.write Process.pid }
 
 server = TCPServer.open(port)
+
+puts "#{Time.now} TCP Server listening."
+
 client = server.accept
+puts "#{Time.now} Receiving binary data."
 File.open(file, 'ab') do |file|
   while chunk = client.read(CHUNK_SIZE)
     file.write(chunk)
   end
 end
+
+puts "#{Time.now} Connection closed."
+File.delete(pid_file)
+puts "#{Time.now} Pidfile deleted."
 
